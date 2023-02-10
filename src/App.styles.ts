99df1496aae92552104f57fef42d1bacf01eb907
claytonfortunato <claytonreis10@gmail.com>
2023-02-10 19:16:11 -0300

    feat: first commit

---
+++ src/App.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #17181f;
+  color: #797a81;
+  min-height: 100vh;
+`;
+export const Area = styled.div`
+  background-color: #fff;
+  margin: auto;
+  max-width: 980px;
+  padding: 10px;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as C from "./App.styles";
+
+function App() {
+  return (
+    <C.Container>
+      <C.Area>
+        <h1>AOBA!</h1>
+      </C.Area>
+    </C.Container>
+  );
+}
+
+export default App;
