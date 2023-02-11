3c551e9aebfe0ee8586ae929d97655f55c352f4f
claytonfortunato <claytonreis10@gmail.com>
2023-02-10 21:14:03 -0300

    feat: components list

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export const Container = styled.div`
@@ -6,8 +7,16 @@ export const Container = styled.div`
   min-height: 100vh;
 `;
 export const Area = styled.div`
-  background-color: #fff;
   margin: auto;
   max-width: 980px;
   padding: 10px;
+`;
+
+export const Header = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: #fff;
+  text-align: center;
+  border-bottom: 1px solid #444;
+  padding-bottom: 20px;
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { ListItem } from "./components/ListItem";
 
-function App() {
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Comprar o pão na padaria", done: false },
+    { id: 1, name: "Comprar o pão na padaria", done: true },
+  ]);
+
   return (
     <C.Container>
       <C.Area>
-        <h1>AOBA!</h1>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        {/* Área de adicionar nova tarefa */}
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
       </C.Area>
     </C.Container>
   );
-}
+};
 
 export default App;
